7254da0eb9df4af0412f8d733a06e7245d84d6de
Rohama-Majeed7 <139218403+Rohama-Majeed7@users.noreply.github.com>
2023-11-18 18:49:32 +0500

    change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const items = [...document.querySelectorAll(".point")];
 
 const updateCount = (el) => {
   const value = parseInt(el.dataset.value);
-  const increment = Math.ceil(value / 1000);
+  const increment = Math.ceil(value / 100);
   // const increment = 1;
   let initialValue = 0;
 
@@ -10,12 +10,16 @@ const updateCount = (el) => {
     initialValue += increment;
 
     if (initialValue > value) {
-      el.textContent = `${value}K`;
+      if (value === 30) {
+        el.textContent = `${value}M+`;
+      } else {
+        el.textContent = `${value}+`;
+      }
       clearInterval(increaseCount);
       return;
     }
 
-    el.textContent = `${initialValue}K`;
+    el.textContent = `${initialValue}+`;
   }, 1);
   // console.log(increaseCount);
 };
@@ -23,7 +27,7 @@ const updateCount = (el) => {
 items.forEach((item) => {
   updateCount(item);
 });
-// fixed 
+// fixed
 window.onscroll = function () {
   var navbar = document.querySelector(".nav");
   if (window.pageYOffset > 100) {
